0723cf72e1d31961ce9bc5ba16d65855dd0f6cd4
tarenstrannik <tarenstrannik@gmail.com>
2023-10-24 13:31:57 +0300

    Changed pooler to work with multiple objects

--- Assets/Scripts/ObjectPooler.cs
+++ Assets/Scripts/ObjectPooler.cs
@@ -1,53 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System; //This allows the IComparable Interface
+using UnityEngine.Pool;
+
+//This is the class you will be storing
+//in the different collections. In order to use
+//a collection's Sort() method, this class needs to
+//implement the IComparable interface.
+
 
 public class ObjectPooler : MonoBehaviour
 {
     public static ObjectPooler SharedInstance;
-    private List<GameObject> pooledObjects;
-    [SerializeField] private GameObject objectToPool;
+   
+    [SerializeField] private GameObject[] objectToPool;
+    private Dictionary<GameObject, List<GameObject>> objectsDictionary;
+
     [SerializeField] private int amountToPool=0;
 
     public bool isPoolingFinished = false;
     void Awake()
     {
         SharedInstance = this;
-        //amountToPool = GetComponent<GameManager>().ballsNumber;
+        objectsDictionary = new Dictionary<GameObject, List<GameObject>>();
+        foreach(GameObject gameObj in objectToPool)
+        {
+            objectsDictionary.Add(gameObj, new List<GameObject> {});
+        }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        if(amountToPool ==0)
+        foreach (KeyValuePair<GameObject,List<GameObject>> pair in objectsDictionary)
         {
-            amountToPool = GetComponent<SpawnManager>().MaxObjectCount(objectToPool);
-        }
-        // Loop through list of pooled objects,deactivating them and adding them to the list 
-        pooledObjects = new List<GameObject>();
-        for (int i = 0; i < amountToPool; i++)
-        {
-            GameObject obj = (GameObject)Instantiate(objectToPool);
-            obj.SetActive(false);
-            pooledObjects.Add(obj);
-            obj.transform.SetParent(this.transform); // set as children of Spawn Manager
+
+            if (amountToPool == 0)
+            {
+                amountToPool = GetComponent<SpawnManager>().MaxObjectCount(pair.Key);
+            }
+            // Loop through list of pooled objects,deactivating them and adding them to the list 
+           
+            for (int i = 0; i < amountToPool; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(pair.Key);
+                obj.SetActive(false);
+                pair.Value.Add(obj);
+                obj.transform.SetParent(this.transform); // set as children of Spawn Manager
+            }
         }
         isPoolingFinished = true;
     }
 
-    public GameObject GetPooledObject()
+    public GameObject GetPooledObject(GameObject objectType)
     {
-        // For as many objects as are in the pooledObjects list
-        for (int i = 0; i < pooledObjects.Count; i++)
+        List<GameObject> objList;
+        if (objectsDictionary.TryGetValue(objectType, out objList))
         {
-            // if the pooled objects is NOT active, return that object 
-            if (!pooledObjects[i].activeInHierarchy)
+            // For as many objects as are in the pooledObjects list
+            for (int i = 0; i < objList.Count; i++)
             {
-                return pooledObjects[i];
+                // if the pooled objects is NOT active, return that object 
+                if (!objList[i].activeInHierarchy)
+                {
+                    return objList[i];
+                }
             }
+            // otherwise, return null   
+            return null;
         }
-        // otherwise, return null   
-        return null;
+        else
+        {
+            return null;
+        }
+
+        
     }
 
 }
